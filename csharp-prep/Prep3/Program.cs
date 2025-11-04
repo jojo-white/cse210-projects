c6e816cdfcbd32ea4e001e1c1abc616fcbf2fca5
jojo-white <josephwhite3141@gmail.com>
2025-11-04 16:03:54 -0700

    Did the assignment for Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        string guessString;
+        bool rightNumber = false;
+        while (rightNumber == false)
+        {
+            Console.Write("Guess a number between 1 and 100: ");
+            guessString = Console.ReadLine();
+            int guess = int.Parse(guessString);
+            if (guess > number)
+            {
+                Console.WriteLine("The number is lower.");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("The number is higher.");
+            }
+            else
+            {
+                Console.Write($"You got it right! The number was {number}!");
+                rightNumber = true;
+            }
+        }
     }
 }
