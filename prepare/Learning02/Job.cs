3667219d9a7c794a46b21fbef01c1377e7ca5ea6
jojo-white <josephwhite3141@gmail.com>
2025-11-11 13:38:03 -0700

    Finished the learning assignment for Abstraction, includes Program.cs file, Job.cs file, and Resume.cs file

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,17 @@
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+
+    public int _startYear = -1;
+    public int _endYear = -1;
+
+    public Job()
+    {
+    }
+
+   public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2024;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+
+        Resume resume1 = new Resume();
+
+        resume1._name = "Joseph White";
+        resume1._jobList.Add(job1);
+        resume1._jobList.Add(job2);
+
+
+        resume1.Display();
+        // job1.Display();
+        // job2.Display();
+
+
     }
 }
