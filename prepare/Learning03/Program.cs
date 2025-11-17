445cbead08edc65c1f49d740305f2775b92e91d7
jojo-white <josephwhite3141@gmail.com>
2025-11-17 14:52:55 -0700

    Wrote code for main program of Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 3);
+
+
+        Console.WriteLine(f1.GetFraction());
+        Console.WriteLine(f1.GetDecimal());
+
+        Console.WriteLine(f2.GetFraction());
+        Console.WriteLine(f2.GetDecimal());
+
+        Console.WriteLine(f3.GetFraction());
+        Console.WriteLine(f3.GetDecimal());
+
+        Console.WriteLine(f4.GetFraction());
+        Console.WriteLine(f4.GetDecimal());
+        
     }
 }
